6943ed9cc660e5717fc606e06e1c232771de7e65
alexjun12 <alexjun12@naver.com>
2022-03-25 02:40:47 +0900

    Install Vue Router, Delete Helloworld.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>This is Home</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeVue",
+  components: {},
+  data() {
+    return {
+      temp: "hello",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
