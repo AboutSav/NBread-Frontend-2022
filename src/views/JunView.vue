cbb04cfefa67047f6122fc280d442d8554c3754f
alexjun12 <alexjun12@naver.com>
2022-03-25 16:56:52 +0900

    Add JunView File

---
+++ src/views/JunView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "JunView",
+  components: {},
+  data() {
+    return {
+      temp: "hello",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
