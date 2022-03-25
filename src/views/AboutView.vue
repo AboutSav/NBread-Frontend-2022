388acaebbc3914732d3db5d71fbaf8930c55bdd2
manyyeon <udayeon@naver.com>
2022-03-25 17:58:34 +0900

    dayeon

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page!</h1>
     <form>
       <input type="text" placeholder="test2" />
       <input type="submit" />
