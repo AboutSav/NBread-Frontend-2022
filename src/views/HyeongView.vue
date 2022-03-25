2a583067ed077be1f190d6d577c83cea7fe6494d
HyeongGook <khg5632@gmail.com>
2022-03-25 16:57:17 +0900

    Add HyeongView

---
+++ src/views/HyeongView.vue
@@ -0,0 +1,20 @@
+<template>
+<div></div>
+</template>
+<script>
+export default {
+  Name: '',
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+<style scoped></style>
